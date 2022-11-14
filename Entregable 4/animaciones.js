43f97924c9e8743eea2071be37606c70f290c180
Regina-Peralta <regiperalta01@gmail.com>
2022-11-14 01:55:56 -0300

    se termina el ej 5 y 6

--- Entregable 4/animaciones.js
+++ Entregable 4/animaciones.js
@@ -28,3 +28,57 @@ let hamburguesa = document.querySelector("#hamburguesa");
     }
 
 hamburguesa.addEventListener("click", cruzHamburguesa);
+
+/******5) El H1 debe reaccionar/disolverse y
+volver a aparecer.************************************ */
+var header = document.getElementById('tituloDesvanecerse');
+function fadeOutOnScroll(element) {
+  if (!element) {
+    return;
+  }
+  
+  var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+  var elementHeight = element.offsetHeight;
+  var scrollTop = document.documentElement.scrollTop * 2;
+  
+  var opacity = 1;
+  
+  if (scrollTop > distanceToTop) {
+    opacity = 1 - (scrollTop - distanceToTop) / elementHeight;
+  }
+  
+  if (opacity >= 0) {
+    element.style.opacity = opacity;
+  }
+}
+
+function scrollHandler() {
+  fadeOutOnScroll(header);
+}
+
+window.addEventListener('scroll', scrollHandler);
+
+/******6. Al scrollear en la Página de lanzamiento del juego, 
+las “Cards” deben aparecer con una
+animación desde afuera de la pantalla y posicionarse en el 
+lugar final tal cual el diseño original. Tener en cuenta 
+que es on-scroll.
+****************************/
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
